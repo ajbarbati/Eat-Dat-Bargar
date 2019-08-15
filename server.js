2e88d83c90cd3cd40765ba75b52a636cc8739c2b
Alex B <ajbarbati@gmail.com>
2019-08-14 21:20:13 -0400

    added handlebars stuff

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const express = require('express');
+const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./controllers/burgers_controller.js')
+
+app.use(express.static(process.cwd() + '/public'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+
+app.set('view engine', 'handlebars')
+
+app.use('/', routes)
+
+app.listen(port)
