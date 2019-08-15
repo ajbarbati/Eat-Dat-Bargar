eca9cb0f2a392aa6377a833a9cf560f1a059d1ba
Alex B <ajbarbati@gmail.com>
2019-08-14 22:24:51 -0400

    starting orm

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,9 +5,31 @@ const burger = require('../models/burger.js')
 
 router.get('/', function(req, res) {
   burger.selectAll(function(data) {
-    var hbsObject = {
+    const hbsObject = {
       burgers: data
     }
     res.render('index', hbsObject)
   })
 })
+
+router.post('/burgers', function(req, res) {
+    burger.insertOne([
+      'burger_name'
+    ], [
+      req.body.burger_name
+    ], function(data) {
+      res.redirect('/')
+    })
+  })
+  
+  router.put('/burgers/:id', function(req, res) {
+    const condition = 'id = ' + req.params.id
+  
+    burger.updateOne({
+      devoured: true
+    }, condition, function(data) {
+      res.redirect('/')
+    })
+  })
+ 
+  module.exports = router
